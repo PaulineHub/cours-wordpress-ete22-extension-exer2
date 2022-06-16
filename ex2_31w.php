f9e7b2e0b5f8425ddb5c2f1ff8c676b620b70d3e
pauline <e2194480@cmaisonneuve.qc.ca>
2022-06-16 10:01:30 -0400

    ajout dossier sass

---
+++ ex2_31w.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Extension de l'exercice #2
+ * Plugin URI: http://wordpress.org/plugins/hello-dolly/
+ * Description: 
+ * Author: Pauline Huby
+ * Author URI: http://github.com/paulinehub/cours-wordpress-ete22-extension2
+ */
+
