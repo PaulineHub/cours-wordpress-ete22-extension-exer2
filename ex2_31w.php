fa365f9653868bcd70d9c125fe00d41d8606459e
pauline <e2194480@cmaisonneuve.qc.ca>
2022-06-16 11:51:28 -0400

    ajout boite modale

--- ex2_31w.php
+++ ex2_31w.php
@@ -1,9 +1,32 @@
 <?php
 /**
- * Plugin Name: Extension de l'exercice #2
- * Plugin URI: http://wordpress.org/plugins/hello-dolly/
+ * Plugin Name: exercice2
+ * Plugin URI: http://wordpress.org/plugins/exercice2/
  * Description: 
  * Author: Pauline Huby
  * Author URI: http://github.com/paulinehub/cours-wordpress-ete22-extension2
  */
 
+function sh2_31w_enqueue()
+{
+   $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+   $version_js = filemtime(plugin_dir_path(__FILE__) . "script/message.js");
+
+   wp_enqueue_style('sh2_31w_css', plugin_dir_url(__FILE__) . "/style.css", array(), $version_css);
+   wp_enqueue_script('sh2_31w_js', plugin_dir_url(__FILE__) .  "script/message.js", array(), $version_js, true);
+}
+
+add_action('wp_enqueue_scripts', 'sh2_31w_enqueue');
+
+function genere_adresse()
+{
+   ///////////////////////////////////// HTML
+   $contenu .= "<code class='code_adresse'>";
+   $contenu .= "3800 Rue Sherbrooke E, Montréal, QC H1X 2A2";
+   $contenu .= "</code>";
+   $contenu .= "<button id='mon_bouton'>OK</button>";
+
+   return $contenu;
+}
+
+add_shortcode('adr', 'genere_adresse');
--- script/message.js
+++ script/message.js
@@ -1,11 +1,15 @@
 (function() 
         {
             let elmBoite__modale;
+            let elmBoite__X;
+
             function creer_boite_modale()
             {
-                let elmBoite__modale = document.createElement('div');
-                let elmBoite__X = document.createElement('div');
+                elmBoite__modale = document.createElement('div');
+                elmBoite__X = document.createElement('button');
                 elmBoite__modale.appendChild(elmBoite__X);
+                elmBoite__X.classList.add('boite__X');
+                elmBoite__X.innerHTML = "X";
                 elmBoite__modale.classList.add('boite__modale');
                 let elmBody = document.querySelector('body');
                 elmBody.appendChild(elmBoite__modale);
@@ -14,9 +18,14 @@
 
             //document.querySelector('.boite__modale');
             let elmOK = document.querySelector('#mon_bouton');
+
             elmOK.addEventListener('mousedown', function() {
                 console.log('Bravo le script fonctionne.');
-                elmBoite__modale.classList.add('boite__modale__ouvrir')
+                elmBoite__modale.classList.add('boite__modale__ouvrir');
             });
+
+            elmBoite__X.addEventListener('mousedown', function() {
+                elmBoite__modale.classList.remove('boite__modale__ouvrir')
+            })
 
         })();
