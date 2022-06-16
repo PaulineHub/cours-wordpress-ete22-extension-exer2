05aa2099e7bd07366c58f81d0d11f745ecc77ecf
pauline <e2194480@cmaisonneuve.qc.ca>
2022-06-16 11:28:16 -0400

    correction bug message.js

--- script/message.js
+++ script/message.js
@@ -1,14 +1,22 @@
 (function() 
         {
-            let elmBoite__modale = document.createElement('div');
-            elmBoite__modale.classList.add('boite__modale');
-            let elmBody = document.querySelector('body');
-            elmBody.appendChild(elmBoite__modale);
+            let elmBoite__modale;
+            function creer_boite_modale()
+            {
+                let elmBoite__modale = document.createElement('div');
+                let elmBoite__X = document.createElement('div');
+                elmBoite__modale.appendChild(elmBoite__X);
+                elmBoite__modale.classList.add('boite__modale');
+                let elmBody = document.querySelector('body');
+                elmBody.appendChild(elmBoite__modale);
+            }
+            creer_boite_modale()
 
-            document.querySelector('.boite__modale');
+            //document.querySelector('.boite__modale');
             let elmOK = document.querySelector('#mon_bouton');
             elmOK.addEventListener('mousedown', function() {
-                console.log('Bravo le script fonctionne.')
+                console.log('Bravo le script fonctionne.');
+                elmBoite__modale.classList.add('boite__modale__ouvrir')
             });
 
         })();
